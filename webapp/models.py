d3a63edc4bfe21a8d7109eb14b8c133d5451c684
Alfonso Jimenez <djalfo18@gmail.com>
2020-03-02 16:07:55 -0500

    First commit for Django web application with Django REST Framework installed and services model defined

---
+++ webapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class servicios(models.Model):
+    id_servicio = models.IntegerField(max_length=10)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    url_imagen = models.URLField(max_length=255)
+    dias_semana = models.CharField(max_length=32)
